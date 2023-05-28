311cf7c8d5c6beb301c23755232724a9655514c4
Colin Vaughn <Colinvaughn01@gmail.com>
2023-05-28 06:26:20 -0400

    that code didn't work, back to working code.

--- Fabric/src/main/java/org/variety/variety_aquatic/world/ModEntitySpawns.java
+++ Fabric/src/main/java/org/variety/variety_aquatic/world/ModEntitySpawns.java
@@ -2,85 +2,114 @@ package org.variety.variety_aquatic.world;
 
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.entity.SpawnRestriction;
 import net.minecraft.entity.mob.WaterCreatureEntity;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.Heightmap;
-import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.BiomeKeys;
 import org.variety.variety_aquatic.Entities.ModEntities;
-import org.variety.variety_aquatic.Entities.custom.CrabEntity;
-import org.variety.variety_aquatic.Entities.custom.HermitcrabEntity;
+import org.variety.variety_aquatic.Entities.custom.*;
 import org.variety.variety_aquatic.Util.NewConfig;
 
-import java.util.Arrays;
-import java.util.List;
 
 public class ModEntitySpawns {
 
+
+
+
     public static void addEntitySpawn() {
-        List<RegistryKey<Biome>> waterBiomesList = Arrays.asList(
-                RegistryKey.of(Registry.BIOME_KEY, new Identifier("minecraft:ocean")),
-                RegistryKey.of(Registry.BIOME_KEY, new Identifier("minecraft:warm_ocean")),
-                RegistryKey.of(Registry.BIOME_KEY, new Identifier("minecraft:lukewarm_ocean")),
-                RegistryKey.of(Registry.BIOME_KEY, new Identifier("minecraft:deep_lukewarm_ocean")),
-                RegistryKey.of(Registry.BIOME_KEY, new Identifier("minecraft:deep_ocean"))
-        );
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.OCEAN,BiomeKeys.WARM_OCEAN,BiomeKeys.LUKEWARM_OCEAN,BiomeKeys.DEEP_LUKEWARM_OCEAN,BiomeKeys.DEEP_OCEAN), SpawnGroup.WATER_CREATURE,
+                ModEntities.SHARK, NewConfig.shark_spawnweight, 1, 2);
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.BEACH), SpawnGroup.CREATURE,
+                ModEntities.CRAB, NewConfig.shark_spawnweight, 1, 4);
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.OCEAN,BiomeKeys.WARM_OCEAN,BiomeKeys.LUKEWARM_OCEAN,BiomeKeys.DEEP_LUKEWARM_OCEAN,BiomeKeys.DEEP_OCEAN), SpawnGroup.WATER_CREATURE,
+                ModEntities.CLOWNFISH, NewConfig.clownfish_spawnweight, 1, 2);
 
-        List<RegistryKey<Biome>> otherBiomesList = Arrays.asList(
-                RegistryKey.of(Registry.BIOME_KEY, new Identifier("minecraft:beach")),
-                RegistryKey.of(Registry.BIOME_KEY, new Identifier("minecraft:jungle")),
-                RegistryKey.of(Registry.BIOME_KEY, new Identifier("minecraft:sparse_jungle")),
-                RegistryKey.of(Registry.BIOME_KEY, new Identifier("minecraft:swamp")),
-                RegistryKey.of(Registry.BIOME_KEY, new Identifier("minecraft:mangrove_swamp")),
-                RegistryKey.of(Registry.BIOME_KEY, new Identifier("minecraft:deep_frozen_ocean"))
-        );
-        RegistryKey<Biome>[] waterBiomes = waterBiomesList.toArray(new RegistryKey[0]);
-        RegistryKey<Biome>[] otherBiomes = otherBiomesList.toArray(new RegistryKey[0]);
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.OCEAN,BiomeKeys.WARM_OCEAN,BiomeKeys.LUKEWARM_OCEAN,BiomeKeys.DEEP_LUKEWARM_OCEAN,BiomeKeys.DEEP_OCEAN), SpawnGroup.WATER_CREATURE,
+                ModEntities.WHALESHARK, NewConfig.whaleshark_spawnweight, 1, 2);
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.OCEAN,BiomeKeys.WARM_OCEAN,BiomeKeys.LUKEWARM_OCEAN,BiomeKeys.DEEP_LUKEWARM_OCEAN,BiomeKeys.DEEP_OCEAN), SpawnGroup.WATER_CREATURE,
+                ModEntities.SUNFISH, NewConfig.sunfish_spawnweight, 1, 3);
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.OCEAN,BiomeKeys.WARM_OCEAN,BiomeKeys.LUKEWARM_OCEAN,BiomeKeys.DEEP_LUKEWARM_OCEAN,BiomeKeys.DEEP_OCEAN), SpawnGroup.WATER_CREATURE,
+                ModEntities.OPAH, NewConfig.opah_spawnweight, 1, 1);
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.OCEAN,BiomeKeys.WARM_OCEAN,BiomeKeys.LUKEWARM_OCEAN,BiomeKeys.DEEP_LUKEWARM_OCEAN,BiomeKeys.DEEP_OCEAN), SpawnGroup.WATER_CREATURE,
+                ModEntities.YELLOWFIN, NewConfig.yellowfin_spawnweight, 1, 2);
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.OCEAN,BiomeKeys.WARM_OCEAN,BiomeKeys.LUKEWARM_OCEAN,BiomeKeys.DEEP_LUKEWARM_OCEAN,BiomeKeys.DEEP_OCEAN), SpawnGroup.WATER_CREATURE,
+                ModEntities.CUTTLEFISH, NewConfig.cuttlefish_spawnweight, 1, 2);
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.OCEAN,BiomeKeys.WARM_OCEAN,BiomeKeys.LUKEWARM_OCEAN,BiomeKeys.DEEP_LUKEWARM_OCEAN,BiomeKeys.DEEP_OCEAN), SpawnGroup.WATER_CREATURE,
+                ModEntities.LIONFISH, NewConfig.lionfish_spawnweight, 1, 1);
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.OCEAN,BiomeKeys.WARM_OCEAN,BiomeKeys.LUKEWARM_OCEAN,BiomeKeys.DEEP_LUKEWARM_OCEAN,BiomeKeys.DEEP_OCEAN), SpawnGroup.WATER_CREATURE,
+                ModEntities.SPOTTEDSTINGRAY, NewConfig.spottedstingray_spawnweight, 1, 1);
+
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.OCEAN,BiomeKeys.WARM_OCEAN,BiomeKeys.LUKEWARM_OCEAN,BiomeKeys.DEEP_LUKEWARM_OCEAN,BiomeKeys.DEEP_OCEAN), SpawnGroup.WATER_CREATURE,
+                ModEntities.JELLYFISH, NewConfig.jellyfish_spawnweight, 2, 4);
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.OCEAN,BiomeKeys.WARM_OCEAN,BiomeKeys.LUKEWARM_OCEAN,BiomeKeys.DEEP_LUKEWARM_OCEAN,BiomeKeys.DEEP_OCEAN), SpawnGroup.WATER_CREATURE,
+                ModEntities.MOONJELLY, NewConfig.moonjelly_spawnweight, 1, 1);
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.DEEP_FROZEN_OCEAN,BiomeKeys.DEEP_OCEAN,BiomeKeys.DEEP_FROZEN_OCEAN,BiomeKeys.DEEP_LUKEWARM_OCEAN), SpawnGroup.WATER_CREATURE,
+                ModEntities.GIANTGLOWINGSQUID, NewConfig.giantsquid_spawnweight, 1, 1);
 
 
-        EntityType[] waterEntities = { ModEntities.SHARK, ModEntities.CLOWNFISH, ModEntities.WHALESHARK, ModEntities.SUNFISH, ModEntities.OPAH, ModEntities.YELLOWFIN,
-                ModEntities.CUTTLEFISH, ModEntities.LIONFISH, ModEntities.SPOTTEDSTINGRAY, ModEntities.JELLYFISH, ModEntities.MOONJELLY,
-                ModEntities.GIANTGLOWINGSQUID };
 
-        int[] waterSpawnWeights = { NewConfig.shark_spawnweight, NewConfig.clownfish_spawnweight, NewConfig.whaleshark_spawnweight, NewConfig.sunfish_spawnweight,
-                NewConfig.opah_spawnweight, NewConfig.yellowfin_spawnweight, NewConfig.cuttlefish_spawnweight, NewConfig.lionfish_spawnweight,
-                NewConfig.spottedstingray_spawnweight, NewConfig.jellyfish_spawnweight, NewConfig.moonjelly_spawnweight, NewConfig.giantsquid_spawnweight };
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.JUNGLE,BiomeKeys.SPARSE_JUNGLE,BiomeKeys.SWAMP,BiomeKeys.MANGROVE_SWAMP),SpawnGroup.WATER_CREATURE,
+                ModEntities.PIRANHA, NewConfig.piranha_spawnweight, 3, 5);
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.JUNGLE,BiomeKeys.SPARSE_JUNGLE,BiomeKeys.MANGROVE_SWAMP),SpawnGroup.WATER_CREATURE,
+                ModEntities.BETTA, NewConfig.betta_spawnweight, 3, 5);
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.JUNGLE,BiomeKeys.SPARSE_JUNGLE,BiomeKeys.MANGROVE_SWAMP),SpawnGroup.WATER_CREATURE,
+                ModEntities.TETRA, NewConfig.tetra_spawnweight, 3, 5);
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.DEEP_COLD_OCEAN,BiomeKeys.DEEP_OCEAN,BiomeKeys.DEEP_LUKEWARM_OCEAN,BiomeKeys.DEEP_FROZEN_OCEAN), SpawnGroup.WATER_CREATURE,
+                ModEntities.ANGLERFISH, NewConfig.anglerfish_spawnweight, 1, 5);
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.DEEP_COLD_OCEAN,BiomeKeys.DEEP_OCEAN,BiomeKeys.DEEP_LUKEWARM_OCEAN), SpawnGroup.WATER_CREATURE,
+                ModEntities.OARFISH, NewConfig.oarfish_spawnweight, 1, 1);
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.DEEP_COLD_OCEAN,BiomeKeys.DEEP_OCEAN,BiomeKeys.DEEP_LUKEWARM_OCEAN,BiomeKeys.DEEP_FROZEN_OCEAN), SpawnGroup.WATER_CREATURE,
+                ModEntities.VAMPIRESQUID, NewConfig.vampiresquid_spawnweight, 1, 5);
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.WARM_OCEAN,BiomeKeys.LUKEWARM_OCEAN),SpawnGroup.WATER_CREATURE,
+                ModEntities.SEAHORSE, NewConfig.seahorse_spawnweight, 3, 5);
 
-        int[] minCounts = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1 };
-        int[] maxCounts = { 2, 4, 2, 3, 1, 2, 2, 1, 1, 4, 1, 1 };
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.BEACH), SpawnGroup.CREATURE,
+                ModEntities.HERMITCRAB, NewConfig.hermitcrab_spawnweight, 1, 2);
 
-        for (int i = 0; i < waterEntities.length; i++) {
-            BiomeModifications.addSpawn(BiomeSelectors.includeByKey(waterBiomes), SpawnGroup.WATER_CREATURE, waterEntities[i], waterSpawnWeights[i], minCounts[i], maxCounts[i]);
-        }
+        SpawnRestriction.register(
+                ModEntities.SHARK, SpawnRestriction.Location.IN_WATER, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, WaterCreatureEntity::canMobSpawn);
+        SpawnRestriction.register(
+                ModEntities.GIANTGLOWINGSQUID, SpawnRestriction.Location.IN_WATER, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, GiantsquidEntity::canMobSpawn);
+        SpawnRestriction.register(
+                ModEntities.ANGLERFISH, SpawnRestriction.Location.IN_WATER, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, AnglerFishEntity::canSpawn);
+        SpawnRestriction.register(
+                ModEntities.OARFISH, SpawnRestriction.Location.IN_WATER, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, OarfishEntity::canSpawn);
+        SpawnRestriction.register(
+                ModEntities.VAMPIRESQUID, SpawnRestriction.Location.IN_WATER, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, AnglerFishEntity::canSpawn);
+        SpawnRestriction.register(
+                ModEntities.PIRANHA, SpawnRestriction.Location.IN_WATER, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, PiranhaEntity::canSpawn);
+        SpawnRestriction.register(
+                ModEntities.CLOWNFISH, SpawnRestriction.Location.IN_WATER, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, WaterCreatureEntity::canMobSpawn);
+        SpawnRestriction.register(
+                ModEntities.TETRA, SpawnRestriction.Location.IN_WATER, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, TetraEntity::canSpawn);
 
-        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.BEACH), SpawnGroup.CREATURE, ModEntities.CRAB, NewConfig.shark_spawnweight, 1, 4);
+        SpawnRestriction.register(
+                ModEntities.BETTA, SpawnRestriction.Location.IN_WATER, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, BettaEntity::canSpawn);
+        SpawnRestriction.register(
+                ModEntities.SEAHORSE, SpawnRestriction.Location.IN_WATER, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, WaterCreatureEntity::canSpawn);
 
-        EntityType[] landEntities = { ModEntities.PIRANHA, ModEntities.BETTA, ModEntities.TETRA, ModEntities.ANGLERFISH, ModEntities.OARFISH, ModEntities.VAMPIRESQUID,
-                ModEntities.SEAHORSE };
+        SpawnRestriction.register(
+                ModEntities.WHALESHARK, SpawnRestriction.Location.IN_WATER, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, WaterCreatureEntity::canMobSpawn);
+        SpawnRestriction.register(
+                ModEntities.CRAB, SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, CrabEntity::canSpawn);
+        SpawnRestriction.register(
+                ModEntities.SUNFISH, SpawnRestriction.Location.IN_WATER, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, WaterCreatureEntity::canMobSpawn);
+        SpawnRestriction.register(
+                ModEntities.YELLOWFIN, SpawnRestriction.Location.IN_WATER, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, YellowfinTunaEntity::canTunaSpawn);
+        SpawnRestriction.register(
+                ModEntities.CUTTLEFISH, SpawnRestriction.Location.IN_WATER, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, WaterCreatureEntity::canMobSpawn);
 
-        int[] landSpawnWeights = { NewConfig.piranha_spawnweight, NewConfig.betta_spawnweight, NewConfig.tetra_spawnweight, NewConfig.anglerfish_spawnweight,
-                NewConfig.oarfish_spawnweight, NewConfig.vampiresquid_spawnweight, NewConfig.seahorse_spawnweight, NewConfig.hermitcrab_spawnweight };
+        SpawnRestriction.register(
+                ModEntities.JELLYFISH, SpawnRestriction.Location.IN_WATER, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, WaterCreatureEntity::canMobSpawn);
 
-        for (int i = 0; i < landEntities.length; i++) {
-            BiomeModifications.addSpawn(BiomeSelectors.includeByKey(otherBiomes), SpawnGroup.WATER_CREATURE, landEntities[i], landSpawnWeights[i], 3, 5);
-        }
+        SpawnRestriction.register(
+                ModEntities.MOONJELLY, SpawnRestriction.Location.IN_WATER, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, WaterCreatureEntity::canMobSpawn);
+        SpawnRestriction.register(
+                ModEntities.SPOTTEDSTINGRAY, SpawnRestriction.Location.IN_WATER, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, WaterCreatureEntity::canMobSpawn);
 
-        SpawnRestriction.Location[] spawnLocations = { SpawnRestriction.Location.IN_WATER, SpawnRestriction.Location.ON_GROUND };
-        Heightmap.Type[] heightmaps = { Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES };
+        SpawnRestriction.register(
+                ModEntities.HERMITCRAB, SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, HermitcrabEntity::canMobSpawn);
 
-        for (int i = 0; i < waterEntities.length; i++) {
-            SpawnRestriction.register(waterEntities[i], spawnLocations[0], heightmaps[0], WaterCreatureEntity::canMobSpawn);
-        }
-
-        for (int i = 0; i < landEntities.length; i++) {
-            SpawnRestriction.register(landEntities[i], spawnLocations[0], heightmaps[0], WaterCreatureEntity::canMobSpawn);
-        }
-
-        SpawnRestriction.register(ModEntities.CRAB, spawnLocations[1], heightmaps[0], CrabEntity::canSpawn);
-        SpawnRestriction.register(ModEntities.HERMITCRAB, spawnLocations[1], heightmaps[0], HermitcrabEntity::canMobSpawn);    }
+    }
 }
